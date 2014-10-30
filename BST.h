72faf1c6b2f97eec7f2d860ca5b9ae21cd224643
unknown <hausfesa@EAS-BEN006-B1.it.muohio.edu>
2014-10-30 12:51:33 -0400

    got some methods, yo

--- BST.h
+++ BST.h
@@ -69,4 +69,156 @@ private:
 
 };
 
-#include "BST.ipp"
+/////////////////////////////////////////////////////////////////////////////////////////////////////// #include "BST.ipp"
+#define NULL 0
+#include <string>
+
+template <class Key, class T>
+BST<Key,T>::BST(){
+	
+}
+
+template <class Key, class T>
+BST<Key,T>::~BST(){
+  //TODO
+	// Brinkman suggests:
+	// Create a new private method called removeAll which recursively deletes everything.
+}
+  
+//Return the number of items currently in the SSet
+template <class Key, class T>
+unsigned long BST<Key,T>::size(){
+  //TODO
+  return 0;
+}
+
+template <class Key, class T>
+unsigned long BST<Key,T>::size(Node<Key,T>* r){
+  if (r==NULL) {
+	  return 0;
+  } else {
+	  // Brinkman says:
+	  // We call this TREE TRAVERSAL
+	  // Not only tree traversal access all the nodes, but it does it in order!
+	  // Super cool!!!!! (Also it's basically quicksort)
+	  return 1+size(r->left)+size(r->right);
+  }
+}
+
+//Add a new item, x, with Key k.
+// If an item with Key k already exists, overwrite it
+template <class Key, class T>
+void BST<Key,T>::add(Key k, T x){
+  //TODO
+}
+
+//Remove the item with Key k. If there is no such item, do nothing.
+template <class Key, class T>
+void BST<Key,T>::remove(Key k){
+  //TODO
+}
+
+//Return the item with Key k. 
+// If there is no such item, throw an exception.
+template <class Key, class T>
+T BST<Key,T>::find(Key k){
+
+ T fakeT;
+ return fakeT;
+}
+//Return true if there is an item with Key k in the table. If not,
+// return false
+template <class Key, class T>
+bool BST<Key,T>::keyExists(Key k){
+  //TODO
+  return false;
+}
+
+//If there is a key in the set that is > k,
+// return the first such key. If not, return k
+template <class Key, class T>
+Key BST<Key,T>::next(Key k){
+  //TODO
+  Key fakeKey;
+  return fakeKey;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::next(Key k, Node<Key,T>* r){
+	if (r == NULL) {
+		// TODO: throw exception
+	} 
+	// TODO
+
+  return NULL;
+}
+
+//If there is a key in the set that is < k,
+// return the first such key. If not, return k
+template <class Key, class T>
+Key BST<Key,T>::prev(Key k){
+  //TODO
+  return NULL;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::prev(Key k, Node<Key,T>* r){
+  //TODO
+  return NULL;
+}
+
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::add(Key k, T x, Node<Key,T>* r){
+  //TODO
+  return NULL;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::remove(Key k, Node<Key,T>* r){
+  //TODO
+  return NULL;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::find(Key k, Node<Key,T>* r){
+	if (r == NULL) {
+		// TODO: throw exception
+	} else if (r->k == k) { // We found it!
+		return r;
+	} else if (k < r->k) { // Look right..
+		find(k, r->right);
+	} else if (k > r->k) { // Look left..
+		find(k, r->left);
+	}
+	
+	return NULL;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::max(Node<Key,T>* r){
+	if (r == NULL) {
+
+		// TODO: throw exception
+
+	} else if (r->right == NULL) {
+		return r;
+	} else {
+		return max(r->right);
+	}
+	return NULL;
+}
+
+template <class Key, class T>
+Node<Key,T>* BST<Key,T>::min(Node<Key,T>* r){
+	if (r == NULL) {
+		
+	// TODO: throw exception
+
+	} else if (r->left == NULL) {
+		return r;
+	} else {
+		return min(r->left);
+	}
+	return NULL;
+}
